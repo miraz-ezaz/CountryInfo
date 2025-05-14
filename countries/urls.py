a3249a50cbe2c507398a34b87cce94a7b0517c75
Ejaz Ahmed Miraj <ejaz@mouchak.com>
2025-05-14 23:20:44 +0600

    added the api to List all countries

---
+++ countries/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from countries import views
+
+
+urlpatterns = [
+    path('countries/', views.CountryListAPIView.as_view(), name='country-list'),
+
+]
--- countries/views.py
+++ countries/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from countries.models import Country, Language, Region
+from countries.serializers import CountrySerializer
 
-# Create your views here.
+
+class CountryListAPIView(generics.ListAPIView):
+    queryset = Country.objects.all()
+    serializer_class = CountrySerializer
+
+
